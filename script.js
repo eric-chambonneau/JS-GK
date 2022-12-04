7d5a6a4ea86fee5a2f701145645ae4536e39eebd
eric-chambonneau <101542272+eric-chambonneau@users.noreply.github.com>
2022-12-04 22:08:30 +0100

    Add files via upload

--- script.js
+++ script.js
@@ -12,3 +12,24 @@ var ctx = canvas.getContext('2d');
 
 ctx.fillStyle = 'red';
 ctx.fillRect(10, 10, 100, 100);
+
+document.addEventListener("DOMContentLoaded", function() {
+    function createParagraph() {
+      let para = document.createElement('p');
+      para.textContent = 'Vous avez cliqué sur le bouton!';
+      document.body.appendChild(para);
+    }
+  
+    const buttons = document.querySelectorAll('button');
+  
+    for(let i = 0; i < buttons.length ; i++) {
+      buttons[i].addEventListener('click', createParagraph);
+    }
+  });
+
+  function createParagraph() {
+    let para = document.createElement('p');
+    para.textContent = 'Vous avez cliqué !';
+    document.body.appendChild(para);
+  }
+  
